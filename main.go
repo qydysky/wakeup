e92d3dc9f5e1e6214c19ab2d63c584830a7a82d0
qydysky <qydysky@foxmail.com>
2020-07-27 07:12:54 +0800

    1

---
+++ main.go
@@ -0,0 +1,49 @@
+// +build !linux
+package main
+
+import (
+	"os"
+	"flag"
+	"syscall"
+	"os/exec"
+)
+
+var (
+	path = flag.String("path", "", "exe path.")
+	file *os.File
+)
+
+func main(){
+
+	flag.Parse()
+	
+	if *path == "" {
+		file, _ = os.OpenFile("README.Use.txt",os.O_CREATE|os.O_WRONLY, 0666)
+		file.WriteAt([]byte("-path {your *.exe path}"), 0)
+		file.Close()
+		return
+	}
+
+	_, err := os.Stat(".lock.loop")
+    if err == nil || os.IsExist(err) {
+		file, _ = os.OpenFile("README.Use.txt",os.O_CREATE|os.O_WRONLY, 0666)
+		file.WriteAt([]byte("u must shutdown the exe and then remove .lock.loop file"), 0)
+		file.Close()
+		return
+	}
+
+	file, _ = os.Create(".lock.loop")
+
+	cmd := exec.Command(*path)
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		HideWindow: true,
+		CreationFlags: syscall.CREATE_NEW_PROCESS_GROUP,
+	}
+	cmd.Start()
+	cmd.Wait()
+	
+	file.Close()
+	os.Remove(".lock.loop")
+
+	return
+}
